d89bd207b40478ae6103765cfa53d557f3c6139b
wzj <wenzhaojie@139.com>
2024-01-27 12:01:11 +0800

    Update

--- wzj/web_api.py
+++ wzj/web_api.py
@@ -219,7 +219,7 @@ def main(args):
 
     fastllm.set_threads(args.threads)
     fastllm.set_low_memory(args.low)
-    fastllm.set_device_map(["cuda:0", "cuda:1"])
+    fastllm.set_device_map({"cuda:0" : 10, "cuda:1" : 5, "cpu": 1})
 
 
     global g_model, g_max_batch_size
